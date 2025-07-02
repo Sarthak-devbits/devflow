d5be1f2e555d19b85d739498b1a661e5912a0694
Sarthak-devbits <snai@ciklum.com>
2025-07-02 15:43:07 +0530

    Implement authentication forms for sign-in and sign-up; add validation schemas and UI components

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validation";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+        name: "",
+        username: "",
+      }}
+      onSubmit={(data) => {
+        return Promise.resolve({
+          success: true,
+          data,
+        });
+      }}
+    />
+  );
 };
 
 export default SignUp;
