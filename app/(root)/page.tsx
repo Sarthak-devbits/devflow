b537763ae52d71117e09ef380a45fc17a72efebd
Sarthak-devbits <snai@ciklum.com>
2025-07-22 16:54:05 +0530

    Refactor tag actions and validation; add getTagQuestions function for retrieving questions by tag

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -86,6 +86,7 @@ export default async function Home({ searchParams }: SearchParams) {
         />
       </section>
       <HomeFilter />
+
       <DataRenderer
         success={success}
         error={error}
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,9 +1,12 @@
 import { error } from "console";
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { PaginatedSearchParamsSchema } from "../validation";
+import {
+  GetTagQuestionsSchema,
+  PaginatedSearchParamsSchema,
+} from "../validation";
 import { FilterQuery } from "mongoose";
-import { Tag } from "@/database";
+import { Question, Tag } from "@/database";
 
 export const getTags = async (
   params: PaginatedSearchParams
@@ -71,7 +74,70 @@ export const getTags = async (
         tags: JSON.parse(JSON.stringify(tags)),
         isNext,
       },
-      
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+};
+
+export const getTagQuestions = async (
+  params: GetTagQuestionsParams
+): Promise<
+  ActionResponse<{
+    tag: Tag;
+    questions: Question[];
+    isNext: boolean;
+  }>
+> => {
+  const validationResult = await action({
+    params,
+    schema: GetTagQuestionsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(error) as ErrorResponse;
+  }
+
+  const { page = 1, pageSize = 10, query, tagId } = params;
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = Number(pageSize);
+
+  try {
+    const tag = await Tag.findById(tagId);
+    if (!tag) {
+      throw new Error("Tag not found");
+    }
+    const filterQuery: FilterQuery<typeof Question> = {
+      tags: { $in: [tagId] },
+    };
+
+    if (query) {
+      filterQuery.title = { $regex: query, $options: "i" };
+    }
+    const totalQuestions = await Question.countDocuments(filterQuery);
+    const questions = await Question.find(filterQuery)
+      .select("_id title views answers upvotes downvotes author createdAt")
+      .populate([
+        {
+          path: "author",
+          select: "name image",
+        },
+        {
+          path: "tags",
+          select: "name image",
+        },
+      ])
+      .lean()
+      .skip(skip)
+      .limit(limit);
+    const isNext = skip + questions.length < totalQuestions;
+    return {
+      success: true,
+      data: {
+        tag: tag,
+        questions: JSON.parse(JSON.stringify(questions)),
+        isNext,
+      },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
