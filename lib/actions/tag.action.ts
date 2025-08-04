ea9be4895886205c4be718fa61d623b520ba96eb
Sarthak-devbits <snai@ciklum.com>
2025-08-04 18:40:03 +0530

    implemented right sidebar

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -7,6 +7,7 @@ import {
 } from "../validation";
 import { FilterQuery } from "mongoose";
 import { Question, Tag } from "@/database";
+import dbConnect from "../mongoose";
 
 export const getTags = async (
   params: PaginatedSearchParams
@@ -138,6 +139,24 @@ export const getTagQuestions = async (
         questions: JSON.parse(JSON.stringify(questions)),
         isNext,
       },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+};
+
+export const getTopTags = async (): Promise<ActionResponse<Tag[]>> => {
+  try {
+    await dbConnect();
+    const tags = await Tag.find()
+      .sort({
+        questions: -1,
+      })
+      .limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(tags)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
